bf32bf060c32ef9a800aa4d9747be69999f98fb3
Dannyel Kayke <dannyel_kayke@hotmail.com>
2021-04-14 22:53:20 -0300

    feat(Teste): implementação de função RemoverSequenciaRepetida

--- Teste1/Program.cs
+++ Teste1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Teste1
 {
@@ -9,10 +10,30 @@ namespace Teste1
          * Desenvolva um método que receba a string "abbcbbb". O retorno desse método deverá ser "abcb", 
          * logo o próximo caracter não deverá ser igual ao anterior, o método deve ser capaz de tratar qualquer string.
         */
+        static string RemoverSequenciaRepetida(string str)
+        {
+            if(str.Length <= 1)
+            {
+                return str;
+            }
+
+            StringBuilder novaStr = new StringBuilder();
+            novaStr.Append(str[0]);
+
+            for (int x = 1; x < str.Length; x++)
+            {
+                if (str[x - 1] != str[x])
+                    novaStr.Append(str[x]);
+            }
+
+            return novaStr.ToString();
+        }
 
         static void Main(string[] args)
         {
             String avaliacao = "abbcbbb";
+            Console.WriteLine(RemoverSequenciaRepetida(avaliacao));
         }
+
     }
 }
